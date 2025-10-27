aa4a6dc10b02dd575c782f0273cf1eaf882c9e9a
shi hong <shaquanhong@gmail.com>
2025-10-26 21:14:30 -0400

    implemented amqp and forked and clone reo

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"os"
+	"os/signal"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	conn_str := "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(conn_str)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Connection is sucessfully established")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Connection Closing")
 }
