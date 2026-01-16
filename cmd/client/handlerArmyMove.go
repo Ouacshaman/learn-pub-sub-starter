533990900b487d37ccc56b4b467708e124bc5abb
Shi Hong <shaquanhong@gmail.com>
2026-01-15 21:46:09 -0500

    added ack enum and also ack implementation in both subscribe json and handler in client

--- cmd/client/handlerArmyMove.go
+++ cmd/client/handlerArmyMove.go
@@ -4,11 +4,20 @@ import (
 	"fmt"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 )
 
-func handlerArmyMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
-	return func(army_move gamelogic.ArmyMove) {
+func handlerArmyMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) pubsub.Acktype {
+	return func(army_move gamelogic.ArmyMove) pubsub.Acktype {
 		defer fmt.Println("> ")
-		gs.HandleMove(army_move)
+		res := gs.HandleMove(army_move)
+		switch res {
+		case gamelogic.MoveOutComeSafe, gamelogic.MoveOutcomeMakeWar:
+			return pubsub.Ack
+		case gamelogic.MoveOutcomeSamePlayer:
+			return pubsub.NackDiscard
+		default:
+			return pubsub.NackDiscard
+		}
 	}
 }
--- internal/pubsub/SubscribeJSON.go
+++ internal/pubsub/SubscribeJSON.go
@@ -6,13 +6,21 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+type Acktype int
+
+const (
+	Ack Acktype = iota
+	NackRequeue
+	NackDiscard
+)
+
 func SubscribeJSON[T any](
 	conn *amqp.Connection,
 	exchange,
 	queueName,
 	key string,
 	queueType SimpleQueueType, // an enum to represent "durable" or "transient"
-	handler func(T),
+	handler func(T) Acktype,
 ) error {
 	ch, _, err := DeclareAndBind(conn, exchange, queueName, key, queueType)
 	if err != nil {
@@ -31,8 +39,21 @@ func SubscribeJSON[T any](
 				fmt.Println(err)
 				continue
 			}
-			handler(gen_body)
-			v.Ack(false)
+			ack_out := handler(gen_body)
+
+			switch ack_out {
+			case Ack:
+				v.Ack(false)
+				fmt.Println("Ack")
+			case NackRequeue:
+				v.Nack(false, true)
+				fmt.Println("NackRequeue")
+			case NackDiscard:
+				v.Nack(false, false)
+				fmt.Println("NackDiscard")
+			default:
+				continue
+			}
 		}
 	}()
 
